53791c078f6134339259cbe90d5d414f31f02013
Iman <artificialiman111@gmail.com>
2025-12-24 17:09:31 +0000

    configs

---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+import adapter from '@sveltejs/adapter-static';
+
+const config = {
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html'
+    }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/halashoe-ecommerce' : ''
+    }
+  }
+};
+
+export default config;
